abf233609b4c863255aed45b340c86bed00f6204
Jeong Yoon <dlwjddbs14@naver.com>
2023-04-20 22:52:06 +0900

    chore: project settings

---
+++ src/main/kotlin/com/dbugbackend/jp/kr/DbugBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.dbugbackend.jp.kr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbugBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DbugBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/dbugbackend/jp/kr/DbugBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dbugbackend.jp.kr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbugBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
